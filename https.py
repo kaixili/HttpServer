84fae809cecfbfa3b994fbea09cfa42057b808a1
kaixili <kaixili@hustunique.com>
2014-12-06 06:38:18 +0800

    完善代码,https在443端口简单实现

--- https.py
+++ https.py
@@ -1,20 +1,20 @@
-import ssl, socket, time
+import ssl, socket,time
 context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
 context.load_cert_chain(certfile='server.pem')
                         
 bindsocket = socket.socket()
 bindsocket.bind(('127.0.0.1', 443))
-bindsocket.listen(5)
+bindsocket.listen(1023)
 
-newsocket, fromaddr = bindsocket.accept()
+newsocket, address = bindsocket.accept()
 connstream = context.wrap_socket(newsocket, server_side=True)
 
 try:
     data = connstream.recv(1024)
-    print(data)
-    buf = 'Hi NN%f\n\n\n\n'%time.time()
-    buf = buf.encode()
-    connstream.send(buf)
+    print(data.decode('utf-8') + 'from' + str(address))
+    data_send = 'time is %f\n\n\n\n'%time.time()
+    
+    connstream.send(bytes(data_send,'utf-8'))
 finally:
     connstream.shutdown(socket.SHUT_RDWR)
     connstream.close()
--- version2.py
+++ version2.py
@@ -1,83 +1,71 @@
 import socket
-from socketserver import ThreadingMixIn
+from data2 import content_type, responses_stat  # 2个字典数据
 
-
-content_type={
-    ".js" : "application/x-javascript",
-    "jpg" : "image/jpg",
-    "gif" : "image/gif",
-    "css" : "text/css",
-    "tml" : "text/html"
-}
-
-
-
+def send_bytes(responses, data):
+    client.send(bytes('HTTP/1.1 {0} {1}\r\n'.format(responses, responses_stat[responses][0]), 'UTF-8'))
+    client.send(bytes("Content-Type: {0}\r\n\r\n".format(content_type[target[-3:]]), 'UTF-8'))
+    client.send(bytes(data, 'UTF-8'))
 
 host = ''
 port = 80
-backlog = 255
+backlog = 1023
 size_max = 1024
 
 location = '/home/lkx810/b'
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #创建socket对象，并说明使用的是IPv4(AF_INET，IP version 4)和TCP协议(SOCK_STREAM)。
-s.bind((host,port))
-s.listen(backlog)
+s.bind((host,port))   #绑定端口
+s.listen(backlog)  
 
 while 1:
     try:
-        client, address = s.accept() #监听端口
+        client, address = s.accept()
     except KeyboardInterrupt:
         s.close()
 
-    data = client.recv(size_max)
-    print('{0} From {1}'.format(data, address))
+    data = client.recv(size_max) 
+    print('{0} From {1}'.format(data.decode('utf-8'), address)) 
     
     
     if not data:
         continue
     data_request_address = data.decode('utf-8').split()[1]
-    if data_request_address == '/':
-        data_request_address = '/index.html'
+    if data_request_address[-1] == '/':
+        data_request_address += 'index.html' 
+    elif data_request_address[0] != '/':
+        send_bytes(411, '')   #没有地址字段 返回411 不支持空字段
     
-    #data_request_method = data.decode('utf-8').split()[0]
+    data_request_method = data.decode('utf-8').split()[0]
     #post 请求 留做后面处理
     #分两种情况：application/x-www-form-urlencoded 和 multipart/form-data
     #RFC 2616 以后再看
+    if data_request_method == 'POST':
+        print('\t POST_DATA:' + data.decode('utf-8').split()[-1])
     
     target = location
     for i in data_request_address.split('/')[1:]:
             target = target + '/' + i
-    print('target---->{0}'.format(target))
-    if data:
-        try:
-            try:
-                with open(target) as data_send:
-                    client.send(bytes('HTTP/1.1 200 OK\r\n', 'UTF-8'))
-                    
-                    
-                    
-                    client.send(bytes("Content-Type: {0}\r\n\r\n".format(content_type[target[-3:]]), 'UTF-8'))
-                    client.send(bytes(data_send.read(), 'UTF-8'))
-            except UnicodeDecodeError:
-                #with open(target, 'rb') as data_send:
-                 #   client.send(bytes('HTTP/1.0 200 OK\r\n', 'UTF-8'))
-                  #  client.send(bytes("Content-Type:{0}\r\n\r\n".format(content_type[target[-3:]]), 'UTF-8') + data_send.read())
-               
-                data_send = b'''HTTP/1.1 200 OK
-Content_Type:image/gif/
 
-'''
-                f = open(target, 'rb').read()
-                data_send = data_send + f
-                client.sendall(data_send)
+    try:
+        try:
+            with open(target) as data_send:
+                data_send = data_send.read()
+                send_bytes(200, data_send)
                 
-        except FileNotFoundError:
-            page404 = location + '/404.html'
-            with open(page404) as data_send:
-                client.send(bytes('HTTP/1.1 404 Not Found\r\n', 'UTF-8'))
-                client.send(bytes("Content-Type: text/html\r\n\r\n", 'UTF-8'))
-                client.send(bytes(data_send.read(), 'UTF-8'))
+        except UnicodeDecodeError:
+            data_send = '''HTTP/1.1 200 OK
+Content_Type:{0}
+
+'''.format(content_type[target[-3:]])  #打不开的文件(图片)二进制 sendall发送
+            f = open(target, 'rb').read()
+            data_send = bytes(data_send, 'utf-8') + f
+            client.sendall(data_send)
+                
+    except FileNotFoundError:
+        target = location + '/404.html'
+        with open(target) as data_send:
+            data_send = data_send.read()
+            send_bytes(404, data_send)
     
     client.close()
     print()
